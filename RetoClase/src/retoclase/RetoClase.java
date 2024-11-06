79edcf09b800cf905e8ad27cac5d2449d76fb966
Smith Dominguez <smitcito.20@gmail.com>
2024-11-06 10:01:31 -0500

    Add files via upload

---
+++ RetoClase/src/retoclase/RetoClase.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package retoclase;
+
+/**
+ *
+ * @author Estudiante
+ */
+
+public class RetoClase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Interfaz interfaz = new Interfaz();
+        interfaz.setSize(500,500);
+        interfaz.setVisible(true);
+    }
+    
+}
